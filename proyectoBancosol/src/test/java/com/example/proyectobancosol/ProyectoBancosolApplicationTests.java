1ef1ac944f9c451448e70a1550bd06e5336d1f9b
Urgamz <urgam@uma.es>
2026-04-20 13:44:11 +0200

    commit inicial

---
+++ proyectoBancosol/src/test/java/com/example/proyectobancosol/ProyectoBancosolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectobancosol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBancosolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
