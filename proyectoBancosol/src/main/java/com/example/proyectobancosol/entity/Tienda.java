e903fc3c10195df621bd1c9a377a173d44d78c0c
martavegas02 <martitavegas286@gmail.com>
2026-04-28 15:32:27 +0200

    Añadidas entidades Tienda y Voluntario

---
+++ proyectoBancosol/src/main/java/com/example/proyectobancosol/entity/Tienda.java
@@ -0,0 +1,27 @@
+package com.example.proyectobancosol.responsabletienda.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "TIENDA")
+public class Tienda {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_tienda")
+    private Integer idTienda;
+
+    @Column(name = "id_cadena")
+    private Integer idCadena;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "direccion")
+    private String direccion;
+
+    @Column(name = "cod_postal")
+    private String codPostal;
+}
---
+++ proyectoBancosol/src/main/java/com/example/proyectobancosol/entity/Voluntario.java
@@ -0,0 +1,27 @@
+package com.example.proyectobancosol.responsabletienda.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "VOLUNTARIO")
+public class Voluntario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_voluntario")
+    private Integer idVoluntario;
+
+    @Column(name = "id_colaborador")
+    private Integer idColaborador;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "email")
+    private String email;
+}
