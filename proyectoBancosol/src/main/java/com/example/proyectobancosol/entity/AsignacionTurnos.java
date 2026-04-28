82c49d48aaf02ed6db3b564fbb63811951071811
martavegas02 <martitavegas286@gmail.com>
2026-04-28 15:48:19 +0200

    Entidades hechas

---
+++ proyectoBancosol/src/main/java/com/example/proyectobancosol/entity/AsignacionTurnos.java
@@ -0,0 +1,37 @@
+package com.example.proyectobancosol.responsabletienda.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalTime;
+
+@Entity
+@Data
+@Table(name = "ASIGNACION_TURNOS")
+public class AsignacionTurnos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_asignacion")
+    private Integer idAsignacion;
+
+    @Column(name = "id_campana")
+    private Integer idCampana;
+
+    @Column(name = "id_tienda")
+    private Integer idTienda;
+
+    @Column(name = "id_voluntario")
+    private Integer idVoluntario;
+
+    @Column(name = "dia")
+    private String dia;
+
+    @Column(name = "franja")
+    private String franja;
+
+    @Column(name = "hora_inicio")
+    private LocalTime horaInicio;
+
+    @Column(name = "hora_fin")
+    private LocalTime horaFin;
+}
