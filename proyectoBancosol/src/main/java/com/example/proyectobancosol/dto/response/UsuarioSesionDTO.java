96ede10ac005ee715dca98e11d8411264fdabb48
Jesus Moreno <48439643+Yixus14@users.noreply.github.com>
2026-06-13 18:46:24 +0200

    Update UsuarioSesionDTO.java

--- proyectoBancosol/src/main/java/com/example/proyectobancosol/dto/response/UsuarioSesionDTO.java
+++ proyectoBancosol/src/main/java/com/example/proyectobancosol/dto/response/UsuarioSesionDTO.java
@@ -1,49 +1,16 @@
 package com.example.proyectobancosol.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class UsuarioSesionDTO {    private Integer idUsuario;
     private String email;
     private String nombreCompleto;
     private String rol;
-
-    public UsuarioSesionDTO() {
-    }
-
-    public UsuarioSesionDTO(Integer idUsuario, String email, String nombreCompleto, String rol) {
-        this.idUsuario = idUsuario;
-        this.email = email;
-        this.nombreCompleto = nombreCompleto;
-        this.rol = rol;
-    }
-
-    public Integer getIdUsuario() {
-        return idUsuario;
-    }
-
-    public void setIdUsuario(Integer idUsuario) {
-        this.idUsuario = idUsuario;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNombreCompleto() {
-        return nombreCompleto;
-    }
-
-    public void setNombreCompleto(String nombreCompleto) {
-        this.nombreCompleto = nombreCompleto;
-    }
-
-    public String getRol() {
-        return rol;
-    }
-
-    public void setRol(String rol) {
-        this.rol = rol;
-    }
+    
 }
